b9528a1866da46a331915de1d08bf490d5817b5a
wenning <ht.wenning@foxmail.com>
2018-05-14 10:30:31 +0800

    init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='autovpn',
+    version='0.1',
+    description='auto connect to openvpn by ip server.',
+    author='wenning',
+    scripts=['autovpn']
+)
